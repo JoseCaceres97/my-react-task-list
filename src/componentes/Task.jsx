319f7cdb68733cb3a0efb747b150a75c734abcda
JoseCaceres97 <josemiguelcaceresg@gmail.com>
2023-12-16 19:32:33 -0500

    Proyecto integrador - 1

---
+++ src/componentes/Task.jsx
@@ -0,0 +1,12 @@
+function Task({ task }) {
+    return (
+      <div>
+        <input type="checkbox" checked={task.completed} />
+        <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+          {task.name}
+        </span>
+      </div>
+    );
+  }
+  
+  export default Task;
